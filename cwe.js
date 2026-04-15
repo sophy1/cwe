92054426ca25acb395edc75f6303879fbceed819
sophy1 <jihye.sophy.won@gmail.com>
2026-04-15 14:18:53 +0900

    Remove duplicate NULL_POINTER functions
    Removed duplicate NULL_POINTER functions from cwe.js.

--- cwe.js
+++ cwe.js
@@ -1,15 +1,4 @@
 // This will show examples related with the Common Weakness Enumeration (CWE).
-  function CWE_tt() { // NULL_POINTER
-        var obj;
-        var y = obj.x;
-        console.log(y);
-    }
-
-  function CWE_test() { // NULL_POINTER
-        var obj;
-        var y = obj.x;
-        console.log(y);
-    }
 
     function CWE_476() { // NULL_POINTER
         var obj;
