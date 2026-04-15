574d67db5ebf63a0e8d9475554d16ef1c07a68cb
sophy1 <jihye.sophy.won@gmail.com>
2026-04-15 14:18:02 +0900

    Add CWE_tt function for NULL_POINTER example
    Added CWE_tt function demonstrating NULL_POINTER issue.

--- cwe.js
+++ cwe.js
@@ -1,4 +1,9 @@
 // This will show examples related with the Common Weakness Enumeration (CWE).
+  function CWE_tt() { // NULL_POINTER
+        var obj;
+        var y = obj.x;
+        console.log(y);
+    }
 
   function CWE_test() { // NULL_POINTER
         var obj;
