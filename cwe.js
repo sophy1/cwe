f07a890e3224e72038478e78329692b84278ecdc
sophy1 <jihye.sophy.won@gmail.com>
2026-04-27 20:37:41 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -1,4 +1,9 @@
 // This will show examples related with the Common Weakness Enumeration (CWE).
+    function CWE_476() { // NULL_POINTER
+        var obj;
+        var y = obj.x;
+        console.log(y);
+    }
 
     function CWE_476() { // NULL_POINTER
         var obj;
